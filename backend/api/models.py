53d695bec4dce8475fd9bf4316b4c565ace29174
Tommi Terävä <tommiterava@gmail.com>
2022-01-29 00:34:59 +0200

    Authentication and users

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,18 +2,22 @@ from django.db import models
 
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class User(models.Model):
+    id = models.CharField(max_length=16, unique=True, primary_key=True, db_index=True)
+    username = models.CharField(max_length=32)
+    discriminator = models.IntegerField()
+    avatar_hash = models.CharField(max_length=32)
+    banner_hash = models.CharField(max_length=32)
+    banner_color = models.CharField(max_length=8)
+    accent_color = models.IntegerField()
+
+    access_token = models.CharField(max_length=64)
+    refresh_token = models.CharField(max_length=64)
+    token_type = models.CharField(max_length=32)
+    token_expiration = models.DateTimeField()
+
+    login_hash = models.CharField(max_length=128)
+    login_hash_expiration = models.DateTimeField()
 
     def __str__(self):
-        return self.question_text
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
+        return f"{self.username}#{self.discriminator} - id: {self.id} - expires: {self.login_hash_expiration}"
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,18 +1,52 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+import json
+import os
+import requests
+import secrets
 
-from .models import Question
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpRequest, HttpResponse
+from django.core.exceptions import PermissionDenied, SuspiciousOperation
+
+from .users import add_user
+
+DISCORD_TOKEN_URL = os.getenv('DISCORD_TOKEN_URL')
+DISCORD_REDIRECT_URI = os.getenv('DISCORD_REDIRECT_URI')
 
 
-def index(request):
-    return HttpResponse("Hello world. This is the backend api.")
+def discord_auth(request: HttpRequest):
+    if request.method != 'POST':
+        raise PermissionDenied
 
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    ret = "Question: {} - Published {}".format(question.question_text, question.pub_date)
-    return HttpResponse(ret)
+    if not isinstance(body, str) or 'code=' not in body:
+        raise SuspiciousOperation
 
+    code = body.replace('code=', '').strip()
+    app_id = os.getenv('APP_ID')
+    app_secret = os.getenv('APP_SECRET')
 
-def discord_auth(request):
-    return HttpResponse()
+    data = {
+        'client_id': app_id,
+        'client_secret': app_secret,
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': DISCORD_REDIRECT_URI,
+        'scope': 'identify'
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    r = requests.post(DISCORD_TOKEN_URL, data=data, headers=headers)
+
+    try:
+        r.raise_for_status()
+    except Exception as e:
+        raise SuspiciousOperation
+
+    token_data = json.loads(r.content)
+    login_hash = add_user(token_data)
+
+    return HttpResponse(content=json.dumps({'token': login_hash}))
